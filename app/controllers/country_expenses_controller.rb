f57168d65a2329cda7f889ac0d5444cedc9f2c8f
plaeski <lisa.plaseski@gmail.com>
2016-03-03 02:26:25 -0500

    Fixed issue with Country Expense Controller

--- app/controllers/country_expenses_controller.rb
+++ app/controllers/country_expenses_controller.rb
@@ -1,38 +1,38 @@
 class CountryExpensesController < ActionController::Base
-  def index
-  	@countries = Country.all
-  	@expenses = CountryExpense.all
-  	@country_expenses = []
-  	@countries.each do |country|
-  	  food = country.country_expenses.where(expense_type:"Food")
-  	  daily_cost = country.country_expenses.where(expense_type:"Daily Cost")
-  	  local_transportation = country.country_expenses.where(expense_type:"Food")
-  	  accomodation = country.country_expenses.where(expense_type:"Accommodation")
-	  	ce = Hash.new
-  	 	ce[:name] = country.country
-  	 	ce[:currency] = food[0].currency
-  	 	ce[:daily_cost] = daily_cost[0].total
-  	 	ce[:accomodation] = accomodation[0].total
-  	 	ce[:food] = food[0].total
-  	 	ce[:local_transportation] = local_transportation[0].total
-  	 	@country_expenses.push(ce) 
-  	end
-  	if @countries.empty?
-      render json: @country_expenses.to_json
-    else
-      render json: @country_expenses.to_json
-    end
-  end
+#   def index
+#   	@countries = Country.all
+#   	@expenses = CountryExpense.all
+#   	@country_expenses = []
+#   	@countries.each do |country|
+#   	  food = country.country_expenses.where(expense_type:"Food")
+#   	  daily_cost = country.country_expenses.where(expense_type:"Daily Cost")
+#   	  local_transportation = country.country_expenses.where(expense_type:"Food")
+#   	  accomodation = country.country_expenses.where(expense_type:"Accommodation")
+# 	  	ce = Hash.new
+#   	 	ce[:name] = country.country
+#   	 	ce[:currency] = food[0].currency
+#   	 	ce[:daily_cost] = daily_cost[0].total
+#   	 	ce[:accomodation] = accomodation[0].total
+#   	 	ce[:food] = food[0].total
+#   	 	ce[:local_transportation] = local_transportation[0].total
+#   	 	@country_expenses.push(ce) 
+#   	end
+#   	if @countries.empty?
+#       render json: @country_expenses.to_json
+#     else
+#       render json: @country_expenses.to_json
+#     end
+#   end
 
-   def show
-    @item = Item.where(params[:id])
+#    def show
+#     @item = Item.where(params[:id])
 
-    if @item.empty?
-      render json: { message: "Item number #{params[:id]} does not exist", status: :not_found }
-    else
-      render json: @item.to_json
-    end
-  end
-end
+#     if @item.empty?
+#       render json: { message: "Item number #{params[:id]} does not exist", status: :not_found }
+#     else
+#       render json: @item.to_json
+#     end
+#   end
+# end
   
 end
