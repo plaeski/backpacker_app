025bfda3f6bb01d46056bb18cd37333aa44cf7fa
plaeski <lisa.plaseski@gmail.com>
2016-03-02 23:54:01 -0500

    Add styling to preview invitation page

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -8,10 +8,13 @@ class RegistrationsController < Devise::RegistrationsController
 
   def create
     super
+    if params[:user][:name]
+      @user.name = params[:user][:name]
+      @user.save
+    end
     if params[:trip_id]
     	user = @user.id
     	trip = Trip.find_by(trip_code: params[:trip_id])
-      byebug
     	TripMembership.create(
     		user_id: user,
     		trip_id: trip.id
