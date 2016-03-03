5a50b9215b51a8729171557bd41a10dab65e2290
plaeski <lisa.plaseski@gmail.com>
2016-03-03 08:33:32 -0500

    Heroku settings

--- config/application.rb
+++ config/application.rb
@@ -35,5 +35,6 @@ module RailsDevise
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.serve_static_assets = true
+    config.assets.compile = true
   end
 end
